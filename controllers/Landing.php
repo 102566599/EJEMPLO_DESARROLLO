d6700bdbef16b40b677274a0559f2b43c945751f
1026566599 <dmejianaranjo096@gmail.com>
2024-11-05 19:36:33 -0500

    Archivos de Arranque

---
+++ controllers/Landing.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Landing{
+
+    public function main(){
+        echo "Soy la funcion main (Prinicpal) del Controlador Landing";
+    }
+    }
+
+   
+
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+require_once "controllers/Landing.php";
+$controller = new Landing; 
+$controller-> main();
+
+// echo "Soy el index, erutador a controladores" ;
+
+?>
